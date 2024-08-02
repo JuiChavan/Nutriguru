fcfc4693c36c42e25f07286af4c2aa274f73dd77
VaishnaviSawant <166615783+sawantvaishnavu10@users.noreply.github.com>
2024-08-02 23:52:49 +0530

    Add files via upload

---
+++ Nutriguru/src/main/java/com/app/entity/Appointment.java
@@ -0,0 +1,42 @@
+package com.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Appointment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// One appointment belongs to many time slot
+	@ManyToOne
+	@JoinColumn(name ="time_Slot")
+	private TimeSlot timeSlot;
+
+	// One Appointment belongs to one client
+	/*
+	 * @OneToOne(mappedBy ="bookAppointment",fetch = FetchType.LAZY) private Client
+	 * client;
+	 */
+	// One Appointment belongs to one nutritionist
+	/*
+	 * @OneToOne
+	 * 
+	 * @JoinColumn(name = "nutritionist_Id") private Nutritionist nutritionist;
+	 */
+		
+}
+
